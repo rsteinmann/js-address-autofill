a786f3b50e952f0bfa2f537af7fd7141b15ddb40
Raphael Steinmann <raphael.steinmann@studitemps.de>
2019-12-06 19:42:07 +0100

    Adds jest as testing framework

--- webpack.config.js
+++ webpack.config.js
@@ -17,13 +17,13 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
+                exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env']
                     }
-                },
-                exclude: /node_modules/,
+                }
             }
         ]
     },
