5942f30af2d8aa7bf36ce89e59484873b95db0f5
Raphael Steinmann <raphael.steinmann@studitemps.de>
2019-12-12 14:16:08 +0100

    Optimizes demo script for linting

--- demo/getConfig.js
+++ demo/getConfig.js
@@ -3,7 +3,7 @@ window.config = null
 async function getConfig(callback, url = 'config.json') {
   // Fetch the JSON file
   var xobj = new XMLHttpRequest()
-  xobj.overrideMimeType("application/json")
+  xobj.overrideMimeType('application/json')
   xobj.open('GET', url, true)
   xobj.onreadystatechange = function () {
     if (xobj.readyState === 4 && xobj.status == 200) {
@@ -14,7 +14,7 @@ async function getConfig(callback, url = 'config.json') {
           callback(configData)
         }
       } catch (error) {
-        console.error('Tried to parse cumstom configuration led to an error', error)
+        console.error('Tried to parse custom configuration led to an error:', error)
       }
     } else if (xobj.readyState === 4 && xobj.status != 200){
       console.error('Could not find custom demo configuration script! Please add your "demo/config.json" to make this demo working!')
